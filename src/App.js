64a38e187f356c54d5e9aa4b29f3e54d4f5f6e41
Andrew T <johnatomich@gmail.com>
2021-05-17 12:37:18 -0500

    Add Spinning Coin Logo.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,62 @@
-//import logo from './logo.svg';
-import swirl from './swirl-cropped.png'
+import logo from './elon_cropped_logo.png';
+import swirl from './elon_cropped.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <header className="App-header">
+      <header id={'top'} className="App-header">
+        <br />
+        <h1 className='2p'>AVOID ASS TAXATION!!</h1>
         <img src={swirl} className="App-logo" alt="logo" />
-        <p>
-          You are getting sleeeepyyyy....
-        </p>
+        <h1>
+          If You Don't Want to Have that ASS Taxed...
+        </h1>
+        <h2>You'd better&nbsp;
         <a
           className="App-link"
-          href="https://coinmarketcap.com/"
-          target="_blank"
+          href="#jump"
+          //target="_blank"
           rel="noopener noreferrer"
         >
-          Learn About Cryptocurrency
-        </a>
+          Learn About That Dogecoin Muthafucka!!
+        </a></h2>
       </header>
+      <br />
+      <br />
+      <br />
+      <section id={'jump'}>
+        <h1>NOW YOU'RE FUCKED!!! HAHAHAHA</h1>
+      </section>
     </div>
   );
 }
 
 const NavBar = () =>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav id={'navbar'} style={{ backgroundColor:'#761999', borderStyle:'solid', borderWidth:'5px', borderColor:'#cd44dd', borderRadius:'.75%' }} class="navbar navbar-expand-lg navbar-dark fixed-top">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#top"><img src={logo} style={{ borderRadius:'50%', width:'180%' }} alt="swirl logo"/></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#top">NFTs</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">NFTees</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">NFTs</a></li>
+            <li><a class="dropdown-item" href="#">NFTees</a></li>
             <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#jump">NFTease</a></li>
           </ul>
         </li>
         <li class="nav-item">
