36149d4bb907b86d27519fecb736fb9a17185a4d
Andrew T <johnatomich@gmail.com>
2021-05-17 08:50:54 -0500

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,66 @@
 //import logo from './logo.svg';
-import rainbow from './rainbow-spiral.jpg'
+import swirl from './swirl-cropped.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
-        <img src={rainbow} style={{borderRadius:'50%'}} className="App-logo" alt="logo" />
+        <img src={swirl} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You are getting sleeeepyyyy....
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://coinmarketcap.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn About Cryptocurrency
         </a>
       </header>
-      <Something />
     </div>
   );
 }
 
-const Something = () =><h1>HEEELLOOOOOOOOOOO!!!!!!</h1>
+const NavBar = () =>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"/></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+  </nav>
 
 
 export default App;
